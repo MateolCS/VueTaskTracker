bcba0fe253df706cd79da61bb50bf5b12b9fca48
MateoICS <mateusz.namysl0318@gmail.com>
2022-04-19 22:31:05 +0200

    Created task tracker app

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+    <button @click="$emit('btn-click')" :style="{background: color}" class="btn">{{title}}</button>
+</template>
+
+<script>
+export default{
+    name: 'Button',
+    props: {
+        title: String,
+        color: {
+            type: String,
+            default: '#000'
+        },
+    }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+       <h1>{{title}}</h1> 
+       <Button @btn-click="$emit('toggle-task-form')" :title="showAddTask ? 'Close' : 'Add Task'" :color = "showAddTask ? 'red' : 'green'" />
+    </header>
+</template>
+
+<script>
+
+import Button from './Button'
+
+export default{
+    name: 'Header',
+    props: {
+        title: String,
+        showAddTask: Boolean
+    },
+    components: {
+        Button
+    },
+    emits: ['toggle-task-form']
+}
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
