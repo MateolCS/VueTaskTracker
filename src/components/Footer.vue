05b39d2793992cb43b017b93805655b491bba526
MateoICS <mateusz.namysl0318@gmail.com>
2022-04-20 23:37:06 +0200

    Added vue router

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <footer>
+        <p>Copyright &copy; 2021</p>
+        <router-link to="/about" class="footer__link">About</router-link>
+    </footer>
+</template>
+
+<style scoped>
+a{
+    color: #333;
+}
+
+footer{
+    margin-top: 20px;
+    text-align: center;
+}
+
+.footer__link{
+    text-decoration: none;
+    font-size: 1.5rem;
+}
+</style>
+
+<script>
+    export default {
+        name: 'Footer',
+    }
+
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="content">
+        <h1>Version 1.0.0</h1>
+        <router-link to="/" class="content__link"> Go back</router-link>
+    </div>
+    
+</template>
+<style scoped>
+.content{
+    text-align: center;
+}
+.content__link{
+    text-decoration: none;
+    color: green;
+    font-weight: bolder;
+    font-size: 1.5rem;
+}
+</style>
